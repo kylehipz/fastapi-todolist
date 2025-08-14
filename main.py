1d07df659bb613d99e281099a7238f4ac1f2ea15
kylehipz <31435847+kylehipz@users.noreply.github.com>
2025-08-14 16:48:19 +0800

    4 api and database schema design (#10)
    * dev: Add volume to database
    
    * feat: Add sqlmodel
    
    * feat: Add postgres
    
    * feat: Add model and postgres connection

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 
+from models import SessionDep, Task, create_db_and_tables
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
 @app.get("/")
 async def main_route():
     return {"message": "Kyle Hipz to hoy!"}
+
+
+@app.post("/tasks/")
+async def create_task(task: Task, session: SessionDep):
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+
+    return task
